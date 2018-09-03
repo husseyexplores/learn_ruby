02db57ceba46b59afc578eeff565810b84d6c4a9
husseyexplores <husseyexplores@gmail.com>
2018-09-03 15:03:14 +0500

    Add 05 solution

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,14 @@
 class Book
 # write your code here
+  attr_accessor :title
+
+  def title= (str)
+    dictionary = 'and,or,for,by,with,at,a,an,the,in,on,over,under,after,of,neaer,from,without'.split(',')
+    arr = str.split
+    arr.each do |word|
+      word = word.capitalize! unless dictionary.include?(word)
+    end
+    arr[0].capitalize!
+    @title = arr.join(' ')
+  end
 end
