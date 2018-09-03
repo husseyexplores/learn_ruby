c50d187566cc6c837cd0f3148998420c1904d6f1
husseyexplores <husseyexplores@gmail.com>
2018-09-03 15:43:22 +0500

    Add solution to 06Timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,16 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize sec=0
+    @seconds = sec 
+  end
+
+  def seconds= sec
+    @seconds = sec
+  end
+
+  def time_string
+    Time.at(@seconds).utc.strftime("%H:%M:%S")
+  end
 end
